56b36b11d9c29bce987617f7ea466af889bb38cf
unknown <breno.rodriguesbsilva12@gmail.com>
2024-10-23 16:41:01 -0300

    teste

---
+++ src/pages/Watch/index.js
@@ -0,0 +1,11 @@
+import styles from "./watch.modules.css"
+
+function Watch() {
+    return (
+        <section className="{styles.watch}">
+        <h1>TESTE</h1>
+        </section>
+    );
+}
+
+export default Watch;
